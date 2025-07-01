793bf566966028090360314b4501b614f6eb01ba
Arie <s7925al@gmail.com>
2025-07-01 10:29:11 +0300

    Cleaned up tracked files and updated .gitignore

--- riddles/Mystery.js
+++
@@ -1,7 +0,0 @@
-import { riddle } from "../classes/Riddle.js";
-
-const Mystery = new riddle(2,"Mystery","I sp`eak without a mouth. What am I?","echo")
-
-export{
-    Mystery
-}
--- riddles/multiple .js
+++
@@ -1,4 +0,0 @@
-import { riddle } from "../classes/Riddle.js";
-const multiple = new MultipleChoice(4,"Multiple Choice","What has keys but can't open locks","c")
-
-
