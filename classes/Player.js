ad157c2752f7a7c7539fac1c917d9847f83e91a4
Arie <s7925al@gmail.com>
2025-07-01 10:29:44 +0300

    Cleaned up tracked files and updated .gitignore

---
+++ classes/Player.js
@@ -0,0 +1,17 @@
+class player{
+    constructor(name){
+        this.name = name;
+        this.arr = [];
+    }
+    time(start,end){
+        this.arr.push(end - start);
+    }
+
+    showStats(){
+        const TimeSuccess = `----${this.name}---time:  ${this.arr}` 
+        return TimeSuccess;
+    }
+}
+export{
+    player
+}
---
+++ riddles/multiple .js
@@ -0,0 +1,4 @@
+import { riddle } from "../classes/Riddle.js";
+const multiple = new MultipleChoice(4,"Multiple Choice","What has keys but can't open locks","c")
+
+
